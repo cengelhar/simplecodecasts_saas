3898030e6f7fd062325e2935eee6a8eae302a4c3
Corey Dev <cengelha@gmail.com>
2016-03-27 02:27:44 +0000

    Added validationg and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,7 @@ class ContactsController < ApplicationController
       # Define @contact variable that calls whatever parameters are defined in the private method below
     @contact = Contact.new(contact_params)
     
-      # when the submit button is clicked, it initiates a save method. This if statement determine where the user will get redirected after the click, and if what message will potentiall occur based on the successfullness of the submission
+      # when the submit button is clicked, it automatically initiates a save method. This if statement determine where the user will get redirected after the click, and if what message will potentiall occur based on the successfullness of the submission
     if @contact.save
         # success and danger are special alert bootstrap class that will pre-format the messages. Flash contains a key, value pair
         #  http://getbootstrap.com/components/#alerts
