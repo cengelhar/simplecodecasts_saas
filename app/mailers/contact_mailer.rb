8e3571481544b259577839d463f71fdc0ef3d458
Corey Dev <cengelha@gmail.com>
2016-03-27 04:55:23 +0000

    refresh

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,12 @@
     # ActionMailer::Base is a rails library. So we're creating an ContactMailer class and having it inherit the attributes of the ActionMailer::Base attributes
 class ContactMailer < ActionMailer::Base
-    default to: 'cengelha@gmail.com'
+  default to: 'your@email_address.com'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
     
-    def contact_email(name, email, body)
-        @name = name
-        @email = email
-        @body = body
-        
-        mail(from: email, subject: 'New Comment from contact form' )
-    end
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end
