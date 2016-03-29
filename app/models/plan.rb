7aa524510784a0d76e591aef95c805a38bf5f715
Corey Dev <cengelha@gmail.com>
2016-03-29 12:06:24 +0000

    Added association for plans and users

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,5 +2,8 @@ class Plan < ActiveRecord::Base
     # created base and pro plan via the rails console
         # name: 'basic', price: 0
         # name: 'pro'. price: 10
+        
+        #create relationship to users class
+    has_many :users
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+    #create relationship to plan class
+  belongs_to :plan
 end
