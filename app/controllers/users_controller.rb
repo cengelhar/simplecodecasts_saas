afcd3921a3d77082aa7aec37417ad3e514f642a7
Corey Dev <cengelha@gmail.com>
2016-04-13 13:27:01 +0000

    added comments in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def show
-            # this is id not user_id
+            # this is id not user_id. the id is getting pulled from the query string parameter for the current_user
             # @user is defined to simply store the current_user's id because the profile show route url structure has a syntax of users/id
         @user = User.find( params[:id] )
     end
