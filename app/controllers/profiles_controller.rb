5c18451def0c7cc3668449864e48ab06a897ba97
Corey Dev <cengelha@gmail.com>
2016-04-15 12:34:30 +0000

    Added before filters to check current_user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,11 @@
 class ProfilesController < ApplicationController
+   
+        # authetication to only allow a user to edit their own profile. if a user changes the url user id, it will redirect to the home page 
+    before_action :authenticate_user!
+        # the action at the bottom that checks is the user id in the url is == to the current_user's id
+    before_action :only_current_user
     
-        # to generate initial form for creation
+    # to generate initial form for creation
     def new
         # form where a user can fill out their own profile.
             # create variable that stores current_user's user_id
@@ -9,7 +14,7 @@ class ProfilesController < ApplicationController
         @profile = Profile.new
     end
     
-        # to save to database
+    # to save to database
     def create
        @user = User.find( params[:user_id] )
             # whitelisting new.html.erb form fields to be able to save values to the database from and external source.
@@ -23,20 +28,36 @@ class ProfilesController < ApplicationController
                 # else render the new action (above)
             render action: :new
         end
-    end
-    
+    end 
+
+    # to edit user profile   
     def edit
         @user = User.find( params[:user_id] )
         @profile = @user.profile
     end
     
-    
-    
+    # to update to database
+    def update
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
+            # if profile updates correctly, provide the user with a success message then redirect to their prfofile page
+        if @profile.update_attributes(profile_params)
+            flash[:success] = "Profile Updated!"
+            redirect_to user_path( params[:user_id] )
+        else
+            render action: :edit
+        end
+    end
+        
+    # private whitelisting (used in above actions)
     private
         def profile_params
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
-    end
+        end
+            
+            # checks if user id in url == current_user.id
+        def only_current_user
+            @user = User.find( params[:user_id] )
+            redirect_to(root_url) unless @user == current_user
+        end
 end
-
-
-
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+    
+        # first used in profiles_controller.rb
+    before_action :authenticate_user!
+    
     def show
             # this is id not user_id. the id is getting pulled from the query string parameter for the current_user
             # @user is defined to simply store the current_user's id because the profile show route url structure has a syntax of users/id
