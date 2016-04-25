02cc9380e3f55d4b9881d9804e0226e1dc87fef3
Corey Dev <cengelha@gmail.com>
2016-04-25 11:58:32 +0000

    Added index action and updated link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
         # first used in profiles_controller.rb
     before_action :authenticate_user!
     
+    def index
+    end
+    
     def show
             # this is id not user_id. the id is getting pulled from the query string parameter for the current_user
             # @user is defined to simply store the current_user's id because the profile show route url structure has a syntax of users/id
